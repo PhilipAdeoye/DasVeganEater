67eda5139781dc3b6f20570012604a89b4360c10
Philip Adeoye <semini08@gmail.com>
2018-03-10 23:53:01 -0500

    Added drawables and text details and descriptions for foods

--- app/src/main/java/com/example/philip/dasveganeater/GrainsFragment.java
+++ app/src/main/java/com/example/philip/dasveganeater/GrainsFragment.java
@@ -30,8 +30,15 @@ public class GrainsFragment extends Fragment {
 
         final ArrayList<Food> foods = new ArrayList<>();
 
-        foods.add(new Food("Rice", "Staple all over the world", R.drawable.placeholder, "Grown in many places, rice has become a staple ..."));
-        foods.add(new Food("Oats", "Quaker made these world famous", R.drawable.placeholder, "Grown primarily in temperate conditions, Oats ..."));
+        foods.add(new Food("Whole Rye", "This cereal grain has more nutrients per 100-calorie serving than any other whole grain.", R.drawable.grains_whole_rye, "It has four times more fiber than whole wheat and 50 percent of the daily recommended amount of iron. It also promotes weight loss, helps prevent gallstones and prevents breast cancer."));
+        foods.add(new Food("Quinoa", "OK, this is actually a seed, not a grain", R.drawable.grains_quinoa, "But it’s widely considered a grain, so we’ll include here. It is super nutritious, especially for vegans because it is a complete protein. It is also high in vitamin E and calcium and low in fat. Quinoa is gluten free."));
+        foods.add(new Food("Oats", "Oats help enhance immune response to infection and stabilize blood sugar.", R.drawable.grains_oats, "Oats are gluten free."));
+        foods.add(new Food("Barley", "Barley is a great source of fiber and selenium", R.drawable.grains_barley, "And a good source of phosphorous, copper and manganese. It also helps prevent gallstones."));
+        foods.add(new Food("Millet", "This grain is a good source of manganese, phosphorus and magnesium.", R.drawable.grains_millet, "It helps with the development and repair of body tissue, helps prevent gallstones and protects against breast cancer."));
+        foods.add(new Food("Brown Rice", "Brown rice is an excellent source of manganese and a good source of selenium and magnesium.", R.drawable.grains_brown_rice, "It’s also high in fiber, rich in antioxidants and helps stabilize blood sugar."));
+        foods.add(new Food("Buckwheat", "Also not a grain; it’s technically a fruit seed", R.drawable.grains_buckwheat, "But it is used like one in cooking, and it’s a good substitute for grains because it is gluten free. Buckwheat helps control blood sugar and helps prevent gallstones."));
+        foods.add(new Food("Spelt", "This grain does not seem to cause sensitivities in many people who are intolerant of wheat.", R.drawable.grains_spelt, "It is an excellent source of manganese and a good source of protein, copper and zinc."));
+        foods.add(new Food("Whole Wheat", "This popular grain is rich in B vitamins and vitamin E.", R.drawable.grains_whole_wheat, "It’s also low in fat."));
 
 
         FoodArrayAdapter foodArrayAdapter = new FoodArrayAdapter(getActivity(), foods);
--- app/src/main/java/com/example/philip/dasveganeater/LeafyGreensFragment.java
+++ app/src/main/java/com/example/philip/dasveganeater/LeafyGreensFragment.java
@@ -30,8 +30,15 @@ public class LeafyGreensFragment extends Fragment {
 
         final ArrayList<Food> foods = new ArrayList<>();
 
-        foods.add(new Food("Kale", "Hipster super food"));
-        foods.add(new Food("Bok Choy", "Not sure what this is"));
+        foods.add(new Food("Kale", "Rising star kale certainly deserves its place in the spotlight.", R.drawable.leafy_greens_kale, "Rich in vitamins A, C, K, B6, as well as calcium and magnesium. It definitely earns its title as a 'superfood' — it’s packed with anti-cancer and cholesterol-lowering antioxidants. These antioxidants also promote heart and eye health."));
+        foods.add(new Food("Collards", "Collard greens are a true leafy rockstar.", R.drawable.leafy_greens_collards, "For one thing, a serving of collard greens has more calcium than a glass of milk! They’re also packed with several potent anti-cancer properties, and are perhaps the best leafy green for lowering cholesterol. They’re  also rich in vitamins K, C, & A."));
+        foods.add(new Food("Spinach", "Popeye isn’t the only one raving about the greatness of spinach. ", R.drawable.leafy_greens_spinach, "This leafy green is rich in iron, calcium, and a whole alphabet of vitamins. One property in spinach has been shown to help combat prostate cancer more than any other popular veggie!"));
+        foods.add(new Food("Broccoli Rabe (Rapini)", "Don’t be confused by the name — broccoli rabe, or rapini,  isn’t related to that famous veggie of the name name.", R.drawable.leafy_greens_rapini, "It’s actually related to turnips! Rapini has plenty of vitamin C, as well as K, A, iron and calcium."));
+        foods.add(new Food("Dandelion Greens", "Dandelions are often thought of as a pesky weed.", R.drawable.leafy_greens_dandelion_greens, "But the greens of dandelions aren’t pesky at all — in fact, they’re nutritional powerhouses! Some even consider dandelion greens to be one of the most nutritious vegetable around! In addition to so many of the  nutritional perks and antioxidants  that other leafy greens give you, dandelion greens are great for digestion and combating viruses."));
+        foods.add(new Food("Cabbage", "Hundreds of studies have shown that cabbage is an excellent cancer-combating veggie.", R.drawable.leafy_greens_cabbage, "It’s also great for your digestive track and your heart. Bok choy, a Chinese cousin of cabbage, is also a nutritious — and delicious — leafy green."));
+        foods.add(new Food("Turnip Greens", "The greens on top of the turnip roots are discarded all too often.", R.drawable.leafy_greens_turnip_greens, "And it’s a shame, really, because the greens are nutritional powerhouses! They are exceptionally bitter, a taste that has been linked in part  to its abundant calcium content. Like so many leafy greens on this list, turnip greens are high in vitamins A, K and C. They have plenty of antioxidants and contain plenty of fiber."));
+        foods.add(new Food("Chard", "With colorful stems and bright green leaves, chard is definitely one of the prettiest greens out there.", R.drawable.leafy_greens_chard, "And, with plenty of vitamins A and C, as well as hefty doses of  iron and fiber, its nutritional value is nothing to sneeze at either."));
+
 
         FoodArrayAdapter foodArrayAdapter = new FoodArrayAdapter(getActivity(), foods);
 
